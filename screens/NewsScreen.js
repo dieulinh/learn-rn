495840ecea0cb0150e14bc6a101c5c049a2407f1
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2018-09-04 23:28:57 +0700

    Handle paging news

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -7,9 +7,14 @@ import ErrorHandler from '../services/ErrorHandler'
 
 class NewsScreen extends React.Component {
 
+  loading = false;
+
   state = {
     posts: [],
-    refreshing: false
+    refreshing: false,
+    page: 1,
+    per_page: 5,
+    is_end: false
   };
 
   static navigationOptions = ({ navigation }) => {
@@ -22,41 +27,101 @@ class NewsScreen extends React.Component {
   }
 
   componentDidMount() {
-    _this = this
-    axios.get("http://members-apis.herokuapp.com/api/v1/posts")
-      .then((response) => {
+
+    this._loadNews({}, () => {
+      console.log("Loaded news!!!!");
+    })
+  }
+
+  _loadNews = (params, callback) => {
+
+    let _this = this;
+    _this.loading = true;
+
+    let page = params.page || this.state.page;
+    let { posts, is_end } = this.state;
+
+    params.per_page = this.state.per_page;
+
+    //http://members-apis.herokuapp.com
+    axios.get("http://192.168.1.4:3000/api/v1/posts", {
+      params: params
+    }).then((response) => {
+        _this.loading = false;
+
         json = response.data
 
         if (json.status != 200) { return ErrorHandler.handleApiError(json); }
 
+        // append next page data
+        posts = params.page == 1 ? json.data : [...posts, ...json.data]
+
+        // mark ended
+        is_end = json.data && (json.data.length == 0 || json.data.length < this.state.per_page)
+
+        // rerender lists
         _this.setState({
-          posts: json.data
+          posts: posts,
+          is_end: is_end,
+          page: page
+        }, () => {
+
+          if (callback) {
+            callback()
+          }
         })
 
       })
   }
 
-  _onRefresh = () => {
-    this.setState({refreshing: true});
+  handleRefresh = () => {
+    console.log("onRefresh!!!!");
 
-    setTimeout(() => {
+    this.setState({refreshing: true});
+    let page = 1;
+
+    this._loadNews({
+      page: page
+    }, () => {
+      this.setState({refreshing: false, is_end: false});
+      console.log("onRefresh Loaded!!!!");
+    })
+  }
+
+  handleLoadMore = () => {
+    if (this.loading || this.state.is_end) { return; }
+
+    let page = this.state.page;
+    page += 1;
+
+    this._loadNews({
+      page: page
+    }, () => {
       this.setState({refreshing: false});
-    }, 3000)
+      console.log("handleLoadMore Loaded!!!!");
+    })
+
   }
 
   render() {
+    const { posts, refreshing } = this.state;
+
     return (
       <FlatList
         refreshControl={
           <RefreshControl
             refreshing={ this.state.refreshing }
-            onRefresh={ this._onRefresh }
           />
         }
 
-        data={this.state.posts}
+        data={posts}
         renderItem={ ({ item }) => <NewsCard post={ item } /> }
         keyExtractor={ (item, index) => item.id.toString() }
+        refreshing={refreshing}
+        onRefresh={this.handleRefresh}
+        onEndReached={this.handleLoadMore}
+        onEndReachedThreshold={0}
+        scrollEventThrottle={200}
       />
     );
   }
