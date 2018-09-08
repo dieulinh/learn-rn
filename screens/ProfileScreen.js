0a4d2c536c9cf2392ed22b826f3cdefa0d9fb9d0
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2018-09-09 00:09:46 +0700

    UI profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { Avatar, Icon } from 'react-native-elements';
+import BaseApi from '../apis/BaseApi';
 
 class ProfileScreen extends React.Component {
-  render() {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Profile"
+    }
+  }
+
+  onLogoutPressed = () => {
+
+  }
+
+  renderLogoutButton = () => {
     return (
-      <View>
-        <Text>Hello ProfileScreen</Text>
+      <Button title='Logout' onPress={ this.onLogoutPressed } />
+    )
+  }
+  render() {
+
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.avatarContainer}>
+          <Avatar
+            rounded
+            xlarge
+            source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+            onPress={() => console.log("Works!")}
+            activeOpacity={0.7}
+          />
+
+          <Text style={styles.userName}>Nhan Nguyen</Text>
+          <Text style={styles.title}>Software Engineer</Text>
+        </View>
+        <View style={styles.rowIconContainer}>
+          <Icon containerStyle={styles.icon} size={15} name='email' />
+          <Text>jerryc.nguyen91@gmail.com</Text>
+        </View>
+        <View style={styles.rowIconContainer}>
+          <Icon containerStyle={styles.icon} size={15} name='map' />
+          <Text>123 Hoa Bang</Text>
+        </View>
+        {this.renderLogoutButton()}
       </View>
     )
+  }
+}
+
+const styles = {
+  avatarContainer: {
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  userName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 5
+  },
+  title: {
+    color: '#999'
+  },
+  rowIconContainer: {
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 10
+  },
+  icon: {
+    marginRight: 10
   }
 }
 
