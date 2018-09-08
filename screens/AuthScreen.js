29650168d09061c84a6ca8780a267af94e2ada41
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2018-09-08 09:32:08 +0700

    Style login screen

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-
+import { Button as RNEButton } from 'react-native-elements';
 
 class AuthScreen extends React.Component {
 
@@ -10,36 +10,59 @@ class AuthScreen extends React.Component {
 
     let _this = this;
 
-    this.props.getCurrentUserInfo().then((response) => {
-      console.log("this.props.current_user", this.props.current_user);
+    this.props.getCurrentUserInfo().then(() => {
       if (this.props.current_user) {
         this.props.navigation.navigate("News");
-      } else {
-        this.props.facebookLogin();
       }
 
     })
 
-
     // AsyncStorage.removeItem("app_token");
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.current_user) {
+      this.props.navigation.navigate("News");
+    }
+  }
+
+  onFacebookButtonPress = () => {
+    this.props.facebookLogin();
+  }
 
   render() {
     return (
-      <View>
-        <Text>Hello AuthScreen</Text>
+      <View style={styles.container}>
+        <Text style={styles.logo}>Demo RN App</Text>
+        <RNEButton onPress={this.onFacebookButtonPress} style={styles.buttonFacebook} {...styles.buttonFacebookProps} title='Login via Facebook' icon={{name: 'facebook', type: 'font-awesome'}} />
+        <Text>with ❤️ by Nhan Nguyen</Text>
       </View>
     )
   }
 
 }
 
-const mapStateToProp = ({auth}) => {
-  console.log("mapStateToProp", auth);
+const mapStateToProps = ({ auth }) => {
   return {
     current_user: auth.current_user
   }
 }
 
-export default connect(mapStateToProp, actions)(AuthScreen);
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  logo: {
+    fontSize: 40
+  },
+  buttonFacebook: {
+    width: 260
+  },
+  buttonFacebookProps: {
+    backgroundColor: "#3C5B96",
+    rounded: true
+  }
+}
+export default connect(mapStateToProps, actions)(AuthScreen);
