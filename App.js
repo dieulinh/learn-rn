7a4f1d4852c59c4c9b07b9d7f1a64fd1986c7e0d
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2018-09-09 23:35:50 +0700

    Update bottom tab navigation bar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { createMaterialTopTabNavigator, createBottomTabNavigator, createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -58,6 +59,28 @@ const AppTabs = createBottomTabNavigator({
   News: NewsStack,
   Contacts: ContactStack,
   Profile: ProfileStack
+}, {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'News') {
+          iconName = `home${focused ? '' : ''}`;
+        } else if (routeName === 'Contacts') {
+          iconName = `contacts${focused ? '' : ''}`;
+        } else if (routeName === 'Profile') {
+          iconName = `person${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#3C5B96',
+      inactiveTintColor: 'gray',
+    }
 });
 
 const AppRoutes = createSwitchNavigator({
