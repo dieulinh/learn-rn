b0f0da3e44ff720e00b20bd9114f48b9c43a0cb1
Dieu Linh Nguyen <dieulinh.nguyen@nfq.asia>
2018-09-19 18:23:29 +0700

    WIP add button to detail event

--- components/events/EventItemDetail.js
+++ components/events/EventItemDetail.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View , Button } from 'react-native';
 
 const EventItemDetail = ({event}) => {
   return (
-    <View>
+    <View style={{flex: 1, flexDirection: 'column'}}>
       <Image source={{uri: event.thumb_url}} style={{width: 100, height: 50}} />
+      <View style={{flexDirection: 'row', height: 40 }}>
+        <View style={{flex: 2}}>
+          <Button title='Zusagen' onPress={console.log('a')} />
+        </View>
+        
+
+        <View style={{flex: 2}}>
+          <Button title='Absagen' onPress={console.log('b')} />
+        </View>
+        
+
+      </View>
     </View>
   )
+}
+const styles = {
+  buttonsContainer: {
+
+  }
 }
 
 export { EventItemDetail };
